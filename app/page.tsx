6bb165b9e571649f6fbcda01019c05e9ca5edc85
Yevhenii Riabokon <yevhenii.riabokon@dci-student.org>
2026-02-11 19:19:15 +0100

    added photo

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 export default function Home() {
 	return (
 		<div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
@@ -6,6 +8,15 @@ export default function Home() {
 					<h1 className='max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50'>
 						First Render Test Project NEXT.JS
 					</h1>
+					<div className='mt-8'>
+						<Image
+							src='/screenshot_2025-07-23_121742.png'
+							alt='Screenshot from public folder'
+							width={800}
+							height={600}
+							className='rounded-lg shadow-md'
+						/>
+					</div>
 				</div>
 			</main>
 		</div>
